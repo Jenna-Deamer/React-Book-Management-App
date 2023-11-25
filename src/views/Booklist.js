135d5e0a9be3cc979b057141aa8b7c2128beb06c
Jenna D <200529678@student.georgianc.on.ca>
2023-11-24 19:48:25 -0500

    delete books, displays updated list on both pages

--- src/views/Booklist.js
+++ src/views/Booklist.js
@@ -3,22 +3,34 @@ import { useState, useEffect } from "react";
 function Booklist ({bookList, updateBookList }){
     //remove selectedBook from bookList
 const deleteSelectedBook = (selectedBook) =>{
-  
-    //create new bookArray
+  //loop through bookList to find index of selected book
+  for (let i = 0; i < bookList.length; i++) {
+    if (bookList[i].title === selectedBook.title) {
+          //once found remove that book from new array
+       bookList.splice(i,1);
+       console.log(bookList)
 
-    //remove selected book & append curren tBookList
+       const updatedList = [...bookList]; ///append new bookList to  new array updatedList
+       updateBookList(updatedList); // Update the bookList in the app.js 
+      
+       localStorage.setItem("session", JSON.stringify(updatedList)); // Store updated list as json string in local storage
+       return;
+    }  };
 }
     return(
         <main className="container">
             <h1 className=" text-center pt-4">Your List</h1>
-            
+            <div className="container">
+        <div className="row">
             {bookList.map((book, index) => (
-        <div className="bookCard" key={index}>
-          <h5>{book.title} <button className="btn btn-danger"   onClick={() => deleteSelectedBook(bookList)}><i class="bi bi-x-lg"></i></button></h5>
+        <div className="bookCard col-lg-4 col-md-4 col-sm-6 mb-4 text-center mt-4" key={index}>
+          <h5>{book.title} <button className="btn btn-danger"   onClick={() => deleteSelectedBook(book)}><i class="bi bi-x-lg"></i></button></h5>
           <img src={book.coverImg.thumbnail} alt={book.title}></img>
           <p className="mt-2">By: {book.author}</p>
         </div>
       ))}
+       </div>
+        </div>
             <button className="btn btn-primary">Add New</button>
         </main>
     )
